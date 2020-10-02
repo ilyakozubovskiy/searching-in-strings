2ffac69b18f8ebf7efa8bf73c7ad4d8ccd5177f5
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2020-10-02 15:03:41 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ SearchingInStrings/EndsWith.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+#pragma warning disable SA1611
+#pragma warning disable CA1062
+#pragma warning disable CA1307
+
+namespace SearchingInStrings
+{
+    public static class EndsWith
+    {
+        /// <summary>
+        /// Determines whether the end of this string instance matches the specified character.
+        /// </summary>
+        /// <returns>true if the <paramref name="value"/> parameter matches the end of this string; otherwise, false.</returns>
+        public static bool IsEndsWith(string str, char value)
+        {
+            // TODO #7-1. Implement the method using String.EndsWith instance method.
+            // See String.EndsWith method documentation page: https://docs.microsoft.com/en-us/dotnet/api/system.string.endsswith
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether the end of this string instance matches the specified string.
+        /// </summary>
+        /// <returns>true if the <paramref name="value"/> parameter matches the end of this string; otherwise, false.</returns>
+        public static bool IsEndsWith(string str, string value)
+        {
+            // TODO #7-2. Implement the method using String.EndsWith instance method.
+            // See String.EndsWith method documentation page: https://docs.microsoft.com/en-us/dotnet/api/system.string.endsswith
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether the end of this string instance matches the specified string when compared using the specified comparison option.
+        /// </summary>
+        /// <returns>true if the <paramref name="value"/> parameter matches the end of this string; otherwise, false.</returns>
+        public static bool IsEndsWithStringComparison(string str, string value)
+        {
+            // TODO #7-3. Implement the method using String.EndsWith instance method. Analyze the unit tests, and use the correct StringComparison enumeration value.
+            // See String.EndsWith and StringComparison documentation pages:
+            // * https://docs.microsoft.com/en-us/dotnet/api/system.string.endsswith
+            // * https://docs.microsoft.com/en-us/dotnet/api/system.stringcomparison
+            throw new NotImplementedException();
+        }
+    }
+}
